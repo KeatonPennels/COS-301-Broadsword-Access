5da9b7de2fc33fd32e74530faba4aa537d67cdf1
Keaton <KeatonPennels@users.noreply.github.com>
2017-03-24 14:02:39 +0200

    Uploading user and flash services for success and error messages

---
+++ Nav UP/user.service.js
@@ -0,0 +1,59 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('UserService', Service);
+
+    function Service($http, $q) {
+        var service = {};
+
+        service.GetCurrent = GetCurrent;
+        service.GetAll = GetAll;
+        service.GetById = GetById;
+        service.GetByUsername = GetByUsername;
+        service.Create = Create;
+        service.Update = Update;
+        service.Delete = Delete;
+
+        return service;
+
+        function GetCurrent() {
+            
+        }
+
+        function GetAll() {
+           
+        }
+
+        function GetById(_id) {
+            
+        }
+
+        function GetByUsername(username) {
+
+        }
+
+        function Create(user) {
+            
+        }
+
+        function Update(user) {
+        }
+
+        function Delete(_id) {
+            
+        }
+
+        // private functions
+
+        function handleSuccess(res) {
+            return res.data;
+        }
+
+        function handleError(res) {
+            return $q.reject(res.data);
+        }
+    }
+
+})();
